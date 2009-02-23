dbb501fa220866e5289989fc316fae17fa8a197a
unknown <Administrator@.(none)>
2009-02-23 02:46:38 +0000

    titles scafoold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :titles
+
   map.resources :channels
 
   # The priority is based upon order of creation: first created -> highest priority.
